848b8ade846b81fc343ab187ed5d9910dfa4f5f6
Juan <jnvleite@gmail.com>
2019-09-20 11:58:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalStyles from 'styles/global';
+import Routes from './routes';
+
+const App = () => (
+  <>
+    <Routes />
+    <GlobalStyles />
+  </>
+);
+
+export default App;
