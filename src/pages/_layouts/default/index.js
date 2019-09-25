32200bc80dd1ffbc8081d90392a8db78b5b5961d
Juan <jnvleite@gmail.com>
2019-09-25 16:32:49 -0300

    new styles

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Header from '~/components/Header';
 import * as S from './styles';
 
 export default function DefaultLayout({ children }) {
-  return <S.Container>{children}</S.Container>;
+  return (
+    <S.Container>
+      <Header />
+      {children}
+    </S.Container>
+  );
 }
 
 DefaultLayout.propTypes = {
