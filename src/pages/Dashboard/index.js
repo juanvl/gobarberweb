8f4a4d0b7b2dcfcf18982685a622eabccccbd76d
Juan <jnvleite@gmail.com>
2019-09-26 17:03:29 -0300

    dashboard styling

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
 import * as S from './styles';
 
-const Dashboard = () => <S.Container>Dashboard</S.Container>;
+const Dashboard = () => {
+  return (
+    <S.Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <S.Time past>
+          <strong>08:00</strong>
+          <span>Juan Victor</span>
+        </S.Time>
+        <S.Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </S.Time>
+        <S.Time available>
+          <strong>10:00</strong>
+          <span>Em aberto</span>
+        </S.Time>
+        <S.Time available>
+          <strong>11:00</strong>
+          <span>Em aberto</span>
+        </S.Time>
+      </ul>
+    </S.Container>
+  );
+};
 
 export default Dashboard;
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,3 +1,53 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    button {
+      border: 0;
+      background: 0;
+    }
+
+    strong {
+      color: #fff;
+      font-size: 24px;
+      margin: 0 15px;
+    }
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+    margin-top: 30px;
+  }
+`;
+
+export const Time = styled.div`
+  padding: 20px;
+  border-radius: 4px;
+  background-color: #fff;
+
+  opacity: ${({ past }) => (past ? 0.6 : 1)};
+
+  strong {
+    display: block;
+    color: ${({ available }) => (available ? '#999' : '#7159c1')};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${({ available }) => (available ? '#999' : '#666')};
+  }
+`;
