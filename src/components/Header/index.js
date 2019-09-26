2e954c8b2491db13fafb85b0c0ac241801f60c1d
Juan <jnvleite@gmail.com>
2019-09-26 15:05:32 -0300

    header data

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +8,8 @@ import logo from '~/assets/logo-purple.svg';
 import * as S from './styles';
 
 export default function Header() {
+  const user = useSelector(state => state.user.profile);
+
   return (
     <S.Container>
       <S.Content>
@@ -19,13 +22,10 @@ export default function Header() {
           <Notifications />
           <S.Profile>
             <div>
-              <strong>Juan Victor</strong>
+              <strong>{user.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
-            <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Juan Victor"
-            />
+            <img src={user.avatar.url} alt={user.name} />
           </S.Profile>
         </aside>
       </S.Content>
